ffc92875b947e86a7c53a32b79794a702d2cfc0d
HarithanR <340831221@gapps.yrdsb.ca>
2022-12-20 13:16:34 -0500

    Added comment to MyWorld subclass

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The world where the wizard, the protagonist, lives in.
  * 
  * @author (your name) 
  * @version (a version number or a date)
