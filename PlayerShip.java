b713bf55560121dbeadde4fcc604243b0b605c65
Raveendran <340831221@EDU.YRDSB.NET>
2023-01-03 20:57:31 -0500

    Exception that needs to be fixed

---
+++ PlayerShip.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The ship the user plays as.
+ * 
+ * @Harithan Raveendran 
+ * @January 2023
+ */
+public class PlayerShip extends Actor
+{
+    GreenfootImage playerShip = new GreenfootImage("images/MillionthVector_blueships1");
+    /**
+     * Act - do whatever the PlayerShip wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public PlayerShip()
+    {
+        setImage(playerShip);
+    }
+    
+    public void act()
+    {
+    }
+}
